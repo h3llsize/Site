f391a6f7d0ff82ae5bba061df31254a252e9b77b
h3llsize <53043705+h3llsize@users.noreply.github.com>
2021-10-27 01:44:38 +0500

    zxc update for clowns
    ahahhah

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    loop: 'true',
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+});
